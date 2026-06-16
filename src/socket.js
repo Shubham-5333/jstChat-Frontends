f847b5b7ed62b2a2b047a551921380d2600506f0
shubham <shubhampatil5333@gmail.com>
2026-06-16 23:48:46 +0530

    changes in js file socket

--- src/socket.js
+++ src/socket.js
@@ -8,19 +8,20 @@ let _socket = null;
  * not at module-load time (which would always be null).
  */
 export function getSocket() {
-  if (!_socket) {
-    const token = localStorage.getItem("token");
+    if (!_socket) {
+        const token = localStorage.getItem("token");
 
-    const url = "https://jst-chat.onrender.com";
+        const url = "https://jst-chat.onrender.com";
 
-    console.log("SOCKET URL:", url);
+        console.log("SOCKET TEST 999");
+        console.log("URL =", url);
 
-    _socket = io(url, {
-      auth: { token },
-    });
-  }
+        _socket = io(url, {
+            auth: { token }
+        });
+    }
 
-  return _socket;
+    return _socket;
 }
 
 /**
